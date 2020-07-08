48aecadbcf2810355f9ded237d0e9428b7e38584
EemptyY0 <67151752+EemptyY0@users.noreply.github.com>
2020-07-08 23:24:43 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,82 @@
 'use strict';
 
+//Массив для хранения карточек
+ const dataBase = [];
+
+ //Переменные с классами элементов из html
  const modalAdd = document.querySelector('.modal__add'),
         addAd =  document.querySelector('.add__ad'),
         modalBtnSubmit = document.querySelector('.modal__btn-submit'),
         modalSubmit = document.querySelector('.modal__submit'),
         catalog = document.querySelector('.catalog'),
-        modalItem = document.querySelector('.modal__item');
+        modalItem = document.querySelector('.modal__item'),
+        modalBtnWarning = document.querySelector('.modal__btn-warning');
 
- //Открытие модального окна подачи объявления(и отключение кнопки отправки)
+//Получение всех элементов формы кроме кнопки
+ const elementsModalSubmit = [...modalSubmit.elements]
+     .filter(elem => elem.tagName !== 'BUTTON' || elem.type !== 'submit');
+
+ //Открытие модального окна подачи объявления(и отключение кнопки "Отправить")
 addAd.addEventListener('click', () => {
+
     modalAdd.classList.remove('hide');
     modalBtnSubmit.disabled = true;
-});
-
-//Закрытие модального окна подачи объявления при нажатии за границей модального окна или на крестик
-modalAdd.addEventListener('click', event => {
-
-    if (event.target.closest('.modal__close') || event.target === modalAdd){
-        modalAdd.classList.add('hide');
-        modalSubmit.reset();
-    }
+    document.addEventListener('keydown', closeModal);
+    document.addEventListener('keydown', closeModal);
 });
 
 //Открытие модального окна товара
-catalog.addEventListener('click', (event) => {
+catalog.addEventListener('click', e => {
+    const target = e.target;
 
-    if (event.target.closest('.card')){
+    if (target.closest('.card')){
         modalItem.classList.remove('hide');
+        document.addEventListener('keydown', closeModal);
     }
 });
 
-//Закрытие модального окна товара при нажатии за границей модального окна или на крестик
-modalItem.addEventListener('click', event => {
+//Код для закрытия модальных окон
+const closeModal = function (e) {
+    const target = e.target;
 
-    if (event.target.closest('.modal__close') || event.target === modalItem){
-        modalItem.classList.add('hide');
-    }
-});
-
-//Закрытие модальных окон нажатием на Esc
-document.body.addEventListener('keydown', (event) => {
-
-    if (event.code === 'Escape'){
+    if (target.closest(".modal__close") || target === this) {
+        this.classList.add('hide');
+        if (this === modalAdd) {
+            modalSubmit.reset();
+        }
+    }else if (e.code === 'Escape'){
         modalItem.classList.add('hide');
         modalAdd.classList.add('hide');
+        modalSubmit.reset();
     }
+};
+
+//Закрытие модальных окон при нажатии за границей модального окна или на крестик
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
+
+//Закрытие формы при нажатии "Отправить"
+modalBtnSubmit.addEventListener('click', () => {modalAdd.classList.add('hide')});
+
+//Проверка значений формы
+modalSubmit.addEventListener('input', () => {
+    const validForm =  elementsModalSubmit.every(elem => elem.value);
+
+    modalBtnSubmit.disabled = !validForm;
+    modalBtnWarning.style.display = validForm ? 'none' : '';
 });
+
+//Отправка значений формы в массив dataBase
+modalSubmit.addEventListener('submit', e => {
+    const itemObj = {};
+
+    e.preventDefault();
+
+    for (const elem of elementsModalSubmit) {
+        itemObj[elem.name] = elem.value;
+    }
+    dataBase.push(itemObj);
+    modalSubmit.reset();
+});
+
+
