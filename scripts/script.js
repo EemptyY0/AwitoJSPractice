355cbae562a99a2ab90c243b797fea87d976a462
EemptyY0 <67151752+EemptyY0@users.noreply.github.com>
2020-07-10 18:49:27 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-//Массив для хранения карточек
- const dataBase = [];
+ //Массив для хранения карточек
+ const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
 
  //Переменные с классами элементов из html
  const modalAdd = document.querySelector('.modal__add'),
@@ -10,13 +10,28 @@
         modalSubmit = document.querySelector('.modal__submit'),
         catalog = document.querySelector('.catalog'),
         modalItem = document.querySelector('.modal__item'),
-        modalBtnWarning = document.querySelector('.modal__btn-warning');
+        modalBtnWarning = document.querySelector('.modal__btn-warning'),
+        modalFileInput = document.querySelector('.modal__file-input'),
+        modalFileBtn = document.querySelector('.modal__file-btn'),
+        modalImageAdd = document.querySelector('.modal__image-add');
+
+ //Доступ к изменению текста кнопки и пути до картинки
+ const textFileBtn = modalFileBtn.textContent,
+        srcModalImage = modalImageAdd.src;
 
 //Получение всех элементов формы кроме кнопки
  const elementsModalSubmit = [...modalSubmit.elements]
      .filter(elem => elem.tagName !== 'BUTTON' || elem.type !== 'submit');
 
- //Открытие модального окна подачи объявления(и отключение кнопки "Отправить")
+//Проверка значений формы
+const checkForm = () => {
+    const validForm =  elementsModalSubmit.every(elem => elem.value);
+
+    modalBtnSubmit.disabled = !validForm;
+    modalBtnWarning.style.display = validForm ? 'none' : '';
+};
+
+//Открытие модального окна подачи объявления(и отключение кнопки "Отправить")
 addAd.addEventListener('click', () => {
 
     modalAdd.classList.remove('hide');
@@ -36,18 +51,18 @@ catalog.addEventListener('click', e => {
 });
 
 //Код для закрытия модальных окон
-const closeModal = function (e) {
+const closeModal = e => {
     const target = e.target;
 
-    if (target.closest(".modal__close") || target === this) {
-        this.classList.add('hide');
-        if (this === modalAdd) {
-            modalSubmit.reset();
-        }
-    }else if (e.code === 'Escape'){
+    if (target.closest('.modal__close') || target.classList.contains('modal') || e.code === 'Escape') {
+
         modalItem.classList.add('hide');
         modalAdd.classList.add('hide');
+        document.removeEventListener('keydown', closeModal);
         modalSubmit.reset();
+        checkForm();
+        modalImageAdd.src = srcModalImage;
+        modalFileBtn.textContent = textFileBtn;
     }
 };
 
@@ -56,15 +71,7 @@ modalAdd.addEventListener('click', closeModal);
 modalItem.addEventListener('click', closeModal);
 
 //Закрытие формы при нажатии "Отправить"
-modalBtnSubmit.addEventListener('click', () => {modalAdd.classList.add('hide')});
-
-//Проверка значений формы
-modalSubmit.addEventListener('input', () => {
-    const validForm =  elementsModalSubmit.every(elem => elem.value);
-
-    modalBtnSubmit.disabled = !validForm;
-    modalBtnWarning.style.display = validForm ? 'none' : '';
-});
+modalSubmit.addEventListener('input', checkForm);
 
 //Отправка значений формы в массив dataBase
 modalSubmit.addEventListener('submit', e => {
@@ -75,8 +82,56 @@ modalSubmit.addEventListener('submit', e => {
     for (const elem of elementsModalSubmit) {
         itemObj[elem.name] = elem.value;
     }
+    itemObj.image = infoPhoto.base64;
     dataBase.push(itemObj);
-    modalSubmit.reset();
+    closeModal({target: modalAdd});
+    saveDB();
+    renderCard();
 });
+
+//Сохранение карточек в БД
+const saveDB = () => localStorage.setItem('awito', JSON.stringify(dataBase));
+
+const infoPhoto = {};
+
+//Добавление загруженной картинки в объявление
+modalFileInput.addEventListener('change', e => {
+    const target = e.target;
+    const reader = new FileReader();
+    const file = target.files[0];
+
+    infoPhoto.filename = file.name;
+    infoPhoto.size = file.size;
+    reader.readAsBinaryString(file);
+
+    reader.addEventListener('load',  e => {
+        if (infoPhoto.size < 200000) {
+            modalFileBtn.textContent = infoPhoto.filename;
+            infoPhoto.base64 = btoa(e.target.result);
+            modalImageAdd.src = `data:image/png;base64,${infoPhoto.base64}`;
+        } else {
+            modalFileBtn.textContent = 'Размер файла не должен превышать 2мб'
+            modalFileInput.value = '';
+            checkForm();
+        }
+    })
+});
+
+//Добавление новой карточки в каталог
+const renderCard = () => {
+    catalog.textContent = '';
+    dataBase.forEach((item, i) => {
+        
+        catalog.insertAdjacentHTML('beforeend', `
+            <li class="card" data-id="${i}">
+                <img class="card__image" src="data:image/png;base64,${item.image}" alt="test">
+                <div class="card__description">
+                    <h3 class="card__header">${item.nameItem}</h3>
+                    <div class="card__price">${item.costItem}</div>
+                </div>
+            </li>
+        `);
+    });
+};
 
 
